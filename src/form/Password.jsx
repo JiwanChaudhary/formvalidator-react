ce5c5f6a5b117104eb96780c7b80897a6aa4c40f
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-06-10 16:38:34 +0545

    password match updated

--- src/form/Password.jsx
+++ src/form/Password.jsx
@@ -2,50 +2,54 @@ import React, { useState } from "react";
 import { FormContainer, Label, Input } from "./Form.styled";
 
 const Password = () => {
-  const [inputPassword, setInputPassword] = useState({
-    password: "",
-    confirmPassword: "",
-  });
+  const [inputPassword, setInputPassword] = useState([
+    {
+      password: "",
+      confirmPassword: "",
+    },
+  ]);
 
-  function handleInputText(e) {
-    const { name, value } = e.target;
-    console.log(value);
-    console.log(name);
-
-    setInputPassword((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+  function handleInputText(index, event) {
+    let data = [...inputPassword];
+    data[index][event.target.name] = event.target.value;
+    setInputPassword(data);
   }
 
   return (
     <>
-      <FormContainer>
-        <Label htmlFor="password1">Password</Label>
-        <Input
-          type="password"
-          id="password1"
-          placeholder="Create Password (Min 8 characters)"
-          required
-          pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
-          title="Please include 1 uppercase, 1 lower case and 1 number."
-          value={inputPassword.password}
-          onChange={handleInputText}
-        />
-      </FormContainer>
-      <FormContainer>
-        <Label htmlFor="password2">Confirm Password</Label>
-        <Input
-          type="password"
-          id="password2"
-          name="password"
-          placeholder="Confirm Password"
-          required
-          pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
-          value={inputPassword.password}
-          onChange={handleInputText}
-        />
-      </FormContainer>
+      {inputPassword.map((input, index) => {
+        return (
+          <>
+            <FormContainer key={index}>
+              <Label htmlFor="password1">Password</Label>
+              <Input
+                name="password"
+                type="password"
+                id="password1"
+                placeholder="Create Password (Min 8 characters)"
+                required
+                pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
+                title="Please include 1 uppercase, 1 lower case and 1 number."
+                value={input.password}
+                onChange={(event) => handleInputText(index, event)}
+              />
+            </FormContainer>
+            <FormContainer key={index}>
+              <Label htmlFor="password2">Confirm Password</Label>
+              <Input
+                type="password"
+                id="password2"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+                required
+                pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
+                value={input.confirmPassword}
+                onChange={(event) => handleInputText(index, event)}
+              />
+            </FormContainer>
+          </>
+        );
+      })}
     </>
   );
 };
